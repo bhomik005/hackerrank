2646c5968531b528b7ce206a426a4ebd76e09737
bhomik005 <bhomik005@gmail.com>
2024-03-30 18:08:03 +1100

    added divisible_sum_pairs.cpp

---
+++ 3_month_prep_kit/week1/divisible_sum_pairs.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int divisibleSumPairs(int n, int k, vector<int> ar) {
+  int count = 0;
+  for(int i = 0; i < n - 1; ++i) {
+    for(int j = i + 1; j < n; ++j) {
+      if(((ar[i] + ar[j]) % k) == 0) count++;
+    }
+  }
+  return count;
+}
+
+int main() {
+ vector<int> arr = {1, 2, 3, 4, 5, 6};
+ int n = arr.size();
+ int k = 5;
+ cout << divisibleSumPairs(n, k, arr) << "\n";
+ return 0;
+}
