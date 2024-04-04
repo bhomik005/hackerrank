d2a94e9bb997cb2ebdb622c756082fb9904954c6
bhomik005 <164642634+bhomik005@users.noreply.github.com>
2024-04-04 12:17:15 +1100

    updated grading_students.cpp

--- 3_month_prep_kit/week2/grading_students.cpp
+++ 3_month_prep_kit/week2/grading_students.cpp
@@ -1,4 +1,5 @@
 vector<int> gradingStudents(vector<int> grades) {
+    // O(n) time | O(1) space
     for(int i = 0; i < grades.size(); ++i) {
         if(grades[i] < 38) continue;
         else if((grades[i] % 5) >= 3) {
