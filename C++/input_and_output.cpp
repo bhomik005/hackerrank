8f494629e1782eef63d8e6493526f8a7d9784c3a
bhomik005 <164642634+bhomik005@users.noreply.github.com>
2024-04-07 22:14:47 +1000

    added input_and_output.cpp

---
+++ C++/input_and_output.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int a, b, c;
+    int total = 0;
+    cin >> a >> b >> c;
+    total = a + b + c;
+    cout << total << "\n";
+    return 0;
+}
